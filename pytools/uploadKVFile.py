197d36ccc322e1af58a4bc6b80ffb63bd7d7d260
Jarle Trollebø <aborysa@gmail.com>
2017-12-17 01:03:27 +0100

    Added new uploading tool

---
+++ pytools/uploadKVFile.py
@@ -0,0 +1,44 @@
+# Creats a key value file based on
+import vdf
+import argparse
+import glob
+import subprocess
+import re
+import os
+
+
+
+
+parser = argparse.ArgumentParser(description='LF -> CRLF')
+parser.add_argument('username',help='steam username')
+parser.add_argument('password',help='steam password')
+parser.add_argument('vdf',help='vdf keyvalue file')
+
+optional_args = ['contentfolder','previewfile','title','changenote','publishedfileid']
+
+for i in optional_args:
+  parser.add_argument('-{}'.format(i))
+
+args = parser.parse_args()
+
+username = args.username
+password = args.password
+file = args.vdf
+
+with open(file, "r") as f:
+  d = vdf.load(f)
+
+wsitem = d["workshopitem"]
+
+
+args = vars(args)
+for i in optional_args:
+  if args[i]:
+    v = wsitem[i].format(args[i])
+    wsitem[i] = v
+    print("Setting {} to {}".format(i, v))
+
+with open("./tmp_vdf.vdf", "w") as f:
+  vdf.dump(d, f, pretty=True)
+
+subprocess.call('steamcmd +login {} {} +workshop_build_item {} +quit'.format(username, password, os.path.realpath('./tmp_vdf.vdf')), shell=True)
